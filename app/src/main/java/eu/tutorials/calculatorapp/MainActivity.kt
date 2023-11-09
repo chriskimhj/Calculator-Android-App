6c7973203157536189f54674386c47bd5492e033
Chris Kim <hjchristopher.dev@gmail.com>
2023-11-08 19:32:09 -0800

    Implement all other operators

--- app/src/main/java/eu/tutorials/calculatorapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/calculatorapp/MainActivity.kt
@@ -65,12 +65,43 @@ class MainActivity : AppCompatActivity() {
                     if(prefix.isNotEmpty()) {
                         one = prefix + one
                     }
-                    tvInput?.text = (one.toDouble() - two.toDouble()).toString()
+                    tvInput?.text = removeTrailingZero((one.toDouble() - two.toDouble()).toString())
+                } else if(tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeTrailingZero((one.toDouble() + two.toDouble()).toString())
+                } else if(tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeTrailingZero((one.toDouble() / two.toDouble()).toString())
+                } else if(tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeTrailingZero((one.toDouble() * two.toDouble()).toString())
                 }
             } catch(e: ArithmeticException) {
                 e.printStackTrace()
             }
         }
+    }
+
+    private fun removeTrailingZero(result: String): String {
+        var value = result
+        if(result.contains(".0"))
+            value = result.substring(0, result.length-2)
+        return value
     }
     private fun isOperatorAdded(value: String): Boolean {
         return if(value.startsWith("-")) {
